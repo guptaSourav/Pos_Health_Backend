a08233ab64a807a104d6df58651b3fd22a223fd8
guptaSourav <guptasouray@gmail.com>
2025-02-04 13:21:41 +0530

    Update Doctor.routes.js

--- src/Routes/Doctor.routes.js
+++ src/Routes/Doctor.routes.js
@@ -13,9 +13,9 @@ const express = require("express");
 const router = express.Router();
 
 // Doctor Routes
-router.post("/add-new", accessControl(["admin"]), addDoctor);
-router.patch("/update/:id", accessControl(["admin"]), updateDoctor);
-router.delete("/delete/:id", accessControl(["admin"]), deleteDoctor);
+router.post("/add-new", addDoctor);
+router.patch("/update/:id", updateDoctor);
+router.delete("/delete/:id",  deleteDoctor);
 router.get("/get-all", getAllDoctors);
 router.get("/get-by-id/:id", getDoctorById);
 
