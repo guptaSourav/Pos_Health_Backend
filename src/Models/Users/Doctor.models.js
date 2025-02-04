ff08806c0b11ebba60966327005642b1c75fcfae
guptaSourav <guptasouray@gmail.com>
2025-02-04 13:35:24 +0530

    Update Doctor.models.js

--- src/Models/Users/Doctor.models.js
+++ src/Models/Users/Doctor.models.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const doctorSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: false, unique: true },
     password: { type: String },
     specialty: {
       type: String,
